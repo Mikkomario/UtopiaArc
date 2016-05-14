5bf9d30522134bd6b822b6b29404e9a757b59dd1
Mikkomario <mikko_hilpinen@hotmail.com>
2016-05-14 13:27:35 +0300

    Phase is now a data type

--- src/utopia/arc/io/XmlPhaseRecorder.java
+++ src/utopia/arc/io/XmlPhaseRecorder.java
@@ -3,7 +3,9 @@ package utopia.arc.io;
 import java.io.OutputStream;
 import java.util.Collection;
 
+import utopia.arc.generics.ArcDataType;
 import utopia.arc.resource.Phase;
+import utopia.flow.generics.ValueList;
 
 /**
  * This static class may be used for reading and writing phases into xml data
@@ -24,6 +26,7 @@ public class XmlPhaseRecorder
 	
 	public static void writePhases(Collection<? extends Phase> phases, OutputStream targetStream)
 	{
-		
+		// Wraps the phases into a list, then writes it
+		ValueList list = new ValueList(ArcDataType.PHASE);
 	}
 }
